fd8edb1f9f68d6eade5d20b2e07455771d3c4d96
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-08-09 02:09:39 -0300

    Clase del 8-8

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
-# TS-Expressjs
- 1-Inciamos repo npm
+# TS-Expressjs #
+
+##Instalar:## npm i -g concurrently nodemon
+
+###Y no olvidar###
+**npm install**
+**npm build**
+**npm start**
+
+###1-Inciamos repo npm###
  npm init --yes
-2- Instaamos express
+###2- Instaamos express###
 npm install express
-3-Instalamos ts y los types de express
+###3-Instalamos ts y los types de express###
 npm i -D typescript @types/express @types/node
-4- Creamos el archivo tsconfig.conf
+###4- Creamos el archivo tsconfig.conf###
 npx tsc --init
-5- Creamos el archivo gitignore
+###5- Creamos el archivo gitignore###
 node_modules
 dist
-6- Agregamos dos Scripts en el archivo package.json
+###6- Agregamos dos Scripts en el archivo package.json###
     "scripts": {
     "build": "npx tsc", (npm run build)
     "start": "node dist/index.js" (npm run start)
--- index.ts
+++ index.ts
@@ -1,11 +1,46 @@
 import express, { Express, Request, Response } from "express"
+import { logParam } from './logMiddleware';
 
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
-	res.send ('HOLA DESDE LA API');
+	console.log(req);
 });
 
-app.listen(1000, () => {
-	console.log(`⚡️ SERVER API IS RUNNING http://localhost:1000`);
+app.get('/usuarios/:usuarioId', logParam, (req: Request, res: Response) => {
+	const usuarios: Array<any> = [
+		{
+			id: 1,
+			email: 'usuario1@example.com',
+			nombre: 'Ignacio',
+			apellido: 'Pérez',
+			edad: 28,
+			activo: true,
+		},
+		{
+			id: 2,
+			email: 'usuario2@example.com',
+			nombre: 'María',
+			apellido: 'González',
+			edad: 35,
+			activo: false,
+		},
+	];
+	// Fix con filter
+	const u = usuarios.filter((u) => u.id == req.params.usuarioId);
+
+	if (u.length <= 0) {
+		res.status(500).json({
+			msg: `No se encontro ningun usuario con el id: ${req.params.usuarioId}`,
+		});
+	}
+
+	res.json(u);
 });
+
+app.listen(1001, () => {
+	console.log(`⚡️ SERVER API IS RUNNING http://localhost:1001`);
+});
+
+
+
